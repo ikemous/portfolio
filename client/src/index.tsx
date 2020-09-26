10b4de12ae683b198f094f4e086bb7f1ef1064ba
ike barranco <barranco.ike@outlook.com>
2020-09-26 09:35:49 -0600

    added router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import allReducers from './utils/reducers';
 import { Provider } from "react-redux";
-import { compose, createStore } from "redux"
+import { createStore } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension";
 import * as serviceWorker from './serviceWorker';
 import './index.css';
@@ -23,4 +23,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
