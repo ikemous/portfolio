1050813d8994012dbf820de35871b7dabe100f9c
Ike <barranco.ike@gmail.com>
2020-09-27 00:28:22 -0600

    started adding contents to home page and navbar

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Container, Image, Row } from "react-bootstrap";
 
 function HomePage() {
     return(
-        <>
-        </>
+        <Container>
+            <Row style={{background:"green"}}>
+                <Image 
+                    style={{margin: "auto"}}
+                    src="https://via.placeholder.com/150"
+                    roundedCircle
+                />
+            </Row>
+            <Row style={{background: "red"}}>
+
+            </Row>
+        </Container>
     );
 };
 
