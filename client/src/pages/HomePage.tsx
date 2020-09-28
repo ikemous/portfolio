3f28f2e98cb40345f7a42fffd8cc0a41967e52ed
Ike <barranco.ike@gmail.com>
2020-09-28 06:08:27 -0600

    integrated Social Links component and replaced list with it

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FollowTheseBro from "../components/FollowTheseBro";
 import { Col, Container, Image, ListGroup, Row } from "react-bootstrap";
 import "./homPage.css";
 
@@ -109,23 +110,7 @@ function HomePage() {
                     <h2>Connect With Me</h2>
                 </Col>
                 <Col xs={12} md={6} className="text-center">
-                    <ListGroup horizontal="lg">
-                        <ListGroup.Item className="socialItem">
-                            <a href="https://www.fb.gg/ikemous" rel="noopener noreferrer" target="_blank">
-                                <h3> <i className="fab fa-facebook" /> Facebook</h3>
-                            </a>
-                        </ListGroup.Item>
-                        <ListGroup.Item className="socialItem">
-                            <a href="https://github.com/ikemous" rel="noopener noreferrer" target="_blank">
-                                <h3><i className="fab fa-github" /> Github</h3>
-                            </a>
-                        </ListGroup.Item>
-                        <ListGroup.Item className="socialItem">
-                            <a href="https://www.linkedin.com/in/ike-barranco/" rel="noopener noreferrer" target="_blank">
-                                <h3><i className="fab fa-linkedin" /> LinkedIn</h3>
-                            </a>
-                        </ListGroup.Item>
-                    </ListGroup>
+                    <FollowTheseBro />
                 </Col>
             </Row>
             <Row 
