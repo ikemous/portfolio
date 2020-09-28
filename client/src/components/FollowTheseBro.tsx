9b6da0ff7b7cd2946525eeede0ed7376292496e0
Ike <barranco.ike@gmail.com>
2020-09-28 06:06:03 -0600

    moved social links to a component

---
+++ client/src/components/FollowTheseBro.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ListGroup } from "react-bootstrap";
+
+function FollowTheseBro() {
+    return (
+        <ListGroup horizontal="lg">
+            <ListGroup.Item className="socialItem">
+                <a href="https://www.fb.gg/ikemous" rel="noopener noreferrer" target="_blank">
+                    <h3> <i className="fab fa-facebook" /> Facebook</h3>
+                </a>
+            </ListGroup.Item>
+            <ListGroup.Item className="socialItem">
+                <a href="https://github.com/ikemous" rel="noopener noreferrer" target="_blank">
+                    <h3><i className="fab fa-github" /> Github</h3>
+                </a>
+            </ListGroup.Item>
+            <ListGroup.Item className="socialItem">
+                <a href="https://www.linkedin.com/in/ike-barranco/" rel="noopener noreferrer" target="_blank">
+                    <h3><i className="fab fa-linkedin" /> LinkedIn</h3>
+                </a>
+            </ListGroup.Item>
+        </ListGroup>
+    );
+};  
+
+export default FollowTheseBro;
