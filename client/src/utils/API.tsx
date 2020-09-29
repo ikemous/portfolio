b703028cf3a755b3f61a0bd9dc2cc394fa39328f
Ike <barranco.ike@gmail.com>
2020-09-29 02:50:34 -0600

    changed API call to string literal

--- client/src/utils/API.tsx
+++ client/src/utils/API.tsx
@@ -4,7 +4,7 @@ export default {
     getProjects() {
         return axios.get("/projects");
     },
-    getFilteredProjects() {
-        return axios.get("/projects/:filter")
+    getFilteredProjects(filter:string) {
+        return axios.get(`/projects/:${filter}`)
     },
 };
