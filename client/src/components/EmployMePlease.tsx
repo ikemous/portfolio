5a40696d9f8af4461ab443985d0223ecc69b56d2
Ike <barranco.ike@gmail.com>
2020-10-01 23:08:17 -0600

    added links to the resumes

--- client/src/components/EmployMePlease.tsx
+++ client/src/components/EmployMePlease.tsx
@@ -8,12 +8,12 @@ function EmployMePlease() {
             <Col  xs={3} />
             <Col xs={3} className="wordResume">
                 <h3>
-                    <a><i className="fas fa-file-word"></i> Word</a>
+                    <a href="/files/resume.docx" rel="noopener noreferrer" target="_blank"><i className="fas fa-file-word"></i> Word</a>
                 </h3>
             </Col>
             <Col xs={3} className="Resume">
                 <h3>
-                    <a><i className="fas fa-file-pdf"></i> PDF</a>
+                    <a href="/files/resume.pdf" rel="noopener noreferrer" target="_blank"><i className="fas fa-file-pdf"></i> PDF</a>
                 </h3>
             </Col>
             <Col  xs={3} />
